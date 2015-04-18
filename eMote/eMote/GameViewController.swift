d01ed33da2098cde136c6f03fe207495b6bb1484
OwenMack <Owen.mack@live.com>
2015-04-18 14:20:23 -0700

    Pushing the game so far with a couple of bugs  bugs include  - displaying the wrong pic with the wrong answer

--- eMote/eMote/GameViewController.swift
+++ eMote/eMote/GameViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class GameViewController: UIViewController {
 
-    @IBOutlet weak var imageButton: UIButton!
+    @IBOutlet weak var emotionImage: UIImageView!
     @IBOutlet weak var button1: UIButton!
     @IBOutlet weak var button2: UIButton!
     @IBOutlet weak var button3: UIButton!
@@ -20,38 +20,17 @@ class GameViewController: UIViewController {
     private var emotionClasses = [EmotionClass]()
     private var currentObject: Int!
     private var winningAnswer: String!
-    
-    var blankImage: UIImage!
+    private var blankImage: UIImage!
+    private var DoingRealFaces: Bool!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        var animatedFace = [UIImage]()
-        var realFace = [UIImage]()
+        DoingRealFaces = true
         
-        //declaring all of the emotion classes
-        var happyClass = EmotionClass(newName: "Happy", newEnum: EmotionEnum.HAPPY, newIcon: UIImage(named: "TestImage1")!, newAnimatedFace: animatedFace, newrealFace: realFace)
-        var sadClass = EmotionClass(newName: "Sad", newEnum: EmotionEnum.SAD, newIcon: UIImage(named: "TestImage1")!, newAnimatedFace: animatedFace, newrealFace: realFace)
-        var angeryClass = EmotionClass(newName: "Angery", newEnum: EmotionEnum.ANGERY, newIcon: UIImage(named: "TestImage1")!, newAnimatedFace: animatedFace, newrealFace: realFace)
-        var scaredClass = EmotionClass(newName: "Scared", newEnum: .SCARED, newIcon: UIImage(named: "TestImage1")!, newAnimatedFace: animatedFace, newrealFace: realFace)
-        var disgustedClass = EmotionClass(newName: "Disgusted", newEnum: .DISGUSTED, newIcon: UIImage(named: "TestImage1")!, newAnimatedFace: animatedFace, newrealFace: realFace)
-        var surprisedClass = EmotionClass(newName: "Surprised", newEnum: .SURPRISED, newIcon: UIImage(named: "TestImage1")!, newAnimatedFace: animatedFace, newrealFace: realFace)
-        
-        //adding all of the classes to the array
-        emotionClasses.append(happyClass)
-        emotionClasses.append(sadClass)
-        emotionClasses.append(angeryClass)
-        emotionClasses.append(scaredClass)
-        emotionClasses.append(disgustedClass)
-        emotionClasses.append(surprisedClass)
-        
-        
-        imageButton.setTitle("", forState: .Normal)
-        imageButton.adjustsImageWhenDisabled = false
-        imageButton.setBackgroundImage(UIImage(named: "TestImage1"),forState: .Normal)
+        emotionClasses = SetTheEmotionObjects()
         answerLabel.text = ""
-        
         
         SetInitalButtonLayout()
     }
@@ -60,7 +39,6 @@ class GameViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
     @IBAction func Button1Clicked(sender: AnyObject) {
         AnyButtonClicked(sender as! UIButton)
     }
@@ -74,8 +52,7 @@ class GameViewController: UIViewController {
         AnyButtonClicked(sender as! UIButton)
     }
     
-    
-    private func AnyButtonClicked(clicked: UIButton){
+    func AnyButtonClicked(clicked: UIButton){
         if(clicked.currentTitle == winningAnswer){
             FadeInAnswer("Correct")
         }else{
@@ -83,35 +60,41 @@ class GameViewController: UIViewController {
         }
         var newCorrectButton = NewRandomButton()
         winningAnswer = newCorrectButton.newName
+        println(winningAnswer)
         var randomButtons = GetRandomNumbers(4, max: 4, insertRandomly: newCorrectButton.randomNum)
         SetNewRandomButtons(randomButtons)
         
-        
-        println("winningAnswer is - \(winningAnswer)")
-//        imageButton.setBackgroundImage(newCorrectButton.newImage, forState: .Normal)
+        emotionImage.image = ResizeImage(newCorrectButton.newImage, targetSize: CGSize(width: 120, height: 120))
     }
     
-    private func SetInitalButtonLayout(){
+    func SetInitalButtonLayout(){
         var newCorrectButton = NewRandomButton()
         winningAnswer = newCorrectButton.newName
+        println(winningAnswer)
         var randomButtons = GetRandomNumbers(4, max: 4, insertRandomly: newCorrectButton.randomNum)
         SetNewRandomButtons(randomButtons)
-        
-        println("winningAnswer is - \(winningAnswer)")
-        //        imageButton.setBackgroundImage(newCorrectButton.newImage, forState: .Normal)
+        emotionImage.image = ResizeImage(newCorrectButton.newImage, targetSize: CGSize(width: 120, height: 120))
     }
     
     //will get all the info for the new winning button
-    private func NewRandomButton() -> (randomNum: Int, randomNumImageNum: Int, newName: String){//), newImage: UIImage) {
+    func NewRandomButton() -> (randomNum: Int, randomNumImageNum: Int, newName: String, newImage: UIImage) {
         var random = Int(arc4random_uniform(UInt32(emotionClasses.count)))
-        var randomImageNum = Int(arc4random_uniform(UInt32(emotionClasses[random].GetRealFace().count)))
+        var randomImageNum = 0
+        var newImage : UIImage!
+        if(DoingRealFaces == true){
+            randomImageNum = Int(arc4random_uniform(UInt32(emotionClasses[random].GetRealFace().count)))
+            newImage = emotionClasses[random].GetRealFace()[randomImageNum]
+        }else{
+            randomImageNum = Int(arc4random_uniform(UInt32(emotionClasses[random].GetAnimatedFace().count)))
+            newImage = emotionClasses[random].GetAnimatedFace()[randomImageNum]
+        }
         var newName = emotionClasses[random].GetName()
-        var newImage : UIImage!//= emotionClasses[random].GetRealFace()[randomImageNum]
         
-        return (random,randomImageNum,newName)//,newImage)
+        
+        return (random,randomImageNum,newName,newImage)
     }
     
-    private func SetNewRandomButtons(randomButtons: [Int]) {
+    func SetNewRandomButtons(randomButtons: [Int]) {
         UIView.animateWithDuration(0.0, delay: 0.0, options: UIViewAnimationOptions.CurveEaseOut, animations: {
             self.button1.alpha = 0.0
             self.button2.alpha = 0.0
@@ -133,9 +116,6 @@ class GameViewController: UIViewController {
                     }, completion: nil)
         })
     }
-
-    
-
     
     //Will get random nums
     //amount - how many you want
@@ -179,6 +159,122 @@ class GameViewController: UIViewController {
             })
         }
     
+    func ResizeImage(image: UIImage, targetSize: CGSize) -> UIImage {
+        let size = image.size
+        
+        let widthRatio  = targetSize.width  / image.size.width
+        let heightRatio = targetSize.height / image.size.height
+        
+        // Figure out what our orientation is, and use that to form the rectangle
+        var newSize: CGSize
+        if(widthRatio > heightRatio) {
+            newSize = CGSizeMake(size.width * heightRatio, size.height * heightRatio)
+        } else {
+            newSize = CGSizeMake(size.width * widthRatio,  size.height * widthRatio)
+        }
+        
+        // This is the rect that we've calculated out and this is what is actually used below
+        let rect = CGRectMake(0, 0, newSize.width, newSize.height)
+        
+        // Actually do the resizing to the rect using the ImageContext stuff
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
+        image.drawInRect(rect)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage
+    }
+    
+    func SetTheEmotionObjects()->[EmotionClass]{
+        var theClasses = [EmotionClass]()
+        
+        let numberOfAnimatedHappyFaces = 1
+        let numberOfAnimatedSadFaces = 1
+        let numberOfAnimatedAngeryFaces = 1
+        let numberOfAnimatedScaredFaces = 1
+        let numberOfAnimatedDisgustFaces = 1
+        let numberOfAnimatedSurprisedFaces = 1
+        
+        var animatedHappyFaces     = [UIImage]()
+        var animatedSadFaces       = [UIImage]()
+        var animatedAngeryFaces    = [UIImage]()
+        var animatedScaredFaces    = [UIImage]()
+        var animatedDistedFaces    = [UIImage]()
+        var animatedSurprisedFaces = [UIImage]()
+        
+        for (var i = 1; i <= numberOfAnimatedHappyFaces; i++) {
+            animatedHappyFaces.append(UIImage(named: "AnimatedHappy\(i)")!)
+        }
+        for (var i = 1; i <= numberOfAnimatedSadFaces; i++) {
+            animatedSadFaces.append(UIImage(named: "AnimatedSad\(i)")!)
+        }
+        for (var i = 1; i <= numberOfAnimatedAngeryFaces; i++) {
+            animatedAngeryFaces.append(UIImage(named: "AnimatedAngry\(i)")!)
+        }
+        for (var i = 1; i <= numberOfAnimatedScaredFaces; i++) {
+            animatedScaredFaces.append(UIImage(named: "AnimatedScared\(i)")!)
+        }
+        for (var i = 1; i <= numberOfAnimatedDisgustFaces; i++) {
+            animatedDistedFaces.append(UIImage(named: "AnimatedDisgust\(i)")!)
+        }
+        for (var i = 1; i <= numberOfAnimatedSurprisedFaces; i++ ) {
+            animatedSurprisedFaces.append(UIImage(named: "AnimatedSurprised\(i)")!)
+        }
+        
+        var realHappyFaces = [UIImage]()
+        var realSadFaces = [UIImage]()
+        var realAngeryFaces = [UIImage]()
+        var realScaredFaces = [UIImage]()
+        var realDistedFaces = [UIImage]()
+        var realSurprisedFaces = [UIImage]()
+        
+        let numberOfRealHappyFaces = 2
+        let numberOfRealSadFaces = 2
+        let numberOfRealAngeryFaces = 2
+        let numberOfRealScaredFaces = 3
+        let numberOfRealDisgustFaces = 2
+        let numberOfRealSurprisedFaces = 3
+        
+        for (var i = 1; i <= numberOfRealHappyFaces; i++) {
+            realHappyFaces.append(UIImage(named: "RealHappy\(i)")!)
+        }
+        for (var i = 1; i <= numberOfRealSadFaces; i++) {
+            realSadFaces.append(UIImage(named: "RealSad\(i)")!)
+        }
+        for (var i = 1; i <= numberOfRealAngeryFaces; i++) {
+            realAngeryFaces.append(UIImage(named: "RealAngry\(i)")!)
+        }
+        for (var i = 1; i <= numberOfRealScaredFaces; i++) {
+            realScaredFaces.append(UIImage(named: "RealScared\(i)")!)
+        }
+        for (var i = 1; i <= numberOfRealDisgustFaces; i++) {
+            realDistedFaces.append(UIImage(named: "RealDisgust\(i)")!)
+        }
+        for (var i = 1; i <= numberOfRealSurprisedFaces; i++) {
+            realSurprisedFaces.append(UIImage(named: "RealSuprised\(i)")!)
+        }
+        
+        //declaring all of the emotion classes
+        var happyClass     = EmotionClass(newName: "Happy", newEnum: EmotionEnum.HAPPY, newIcon: animatedHappyFaces[0], newAnimatedFace: animatedHappyFaces, newrealFace: realHappyFaces)
+        var sadClass       = EmotionClass(newName: "Sad", newEnum: EmotionEnum.SAD, newIcon: animatedSadFaces[0], newAnimatedFace: animatedSadFaces, newrealFace: realSurprisedFaces)
+        var angeryClass    = EmotionClass(newName: "Angry", newEnum: EmotionEnum.ANGERY, newIcon: animatedAngeryFaces[0], newAnimatedFace: animatedAngeryFaces, newrealFace: realAngeryFaces)
+        var scaredClass    = EmotionClass(newName: "Scared", newEnum: .SCARED, newIcon: animatedScaredFaces[0], newAnimatedFace: animatedScaredFaces, newrealFace: realScaredFaces)
+        var disgustedClass = EmotionClass(newName: "Disgusted", newEnum: .DISGUSTED, newIcon: animatedDistedFaces[0], newAnimatedFace: animatedDistedFaces, newrealFace: realDistedFaces)
+        var surprisedClass = EmotionClass(newName: "Surprised", newEnum: .SURPRISED, newIcon: animatedSurprisedFaces[0], newAnimatedFace: animatedSurprisedFaces, newrealFace: realSurprisedFaces)
+        
+        //adding all of the classes to the array
+        theClasses.append(happyClass)
+        theClasses.append(sadClass)
+        theClasses.append(angeryClass)
+        theClasses.append(scaredClass)
+        theClasses.append(disgustedClass)
+        theClasses.append(surprisedClass)
+        
+        
+        return theClasses
+    }
+    
+    //Don't know if will need
 //    func FadeOutAnswer(){
 //        println("in the func")
 //        UIView.animateWithDuration(1.0, delay: 0.0, options: UIViewAnimationOptions.CurveEaseOut, animations: {
@@ -186,15 +282,15 @@ class GameViewController: UIViewController {
 //            }, completion: {
 //                (finished: Bool) -> Void in
 //                
-//                self.answerLabel.text = nil
+//                self.answerLabel.texqt = nil
 //                
 //                UIView.animateWithDuration(1.0, delay: 0.0, options: UIViewAnimationOptions.CurveEaseOut, animations: {
 //                    self.answerLabel.alpha = 0.0
 //                    }, completion: nil)
 //        })
 //    }
-    
-    
+//    
+//    
     //Don't know if will need
 //    private func GetRandomNumbers(amount: Int,max: Int) -> [Int]{
 //        var nums = [Int]()
